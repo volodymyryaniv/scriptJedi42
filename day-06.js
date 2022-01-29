ab4b58b348a1e116c05bcc05c446ad076c998533
volodymyryaniv <89596689+volodymyryaniv@users.noreply.github.com>
2022-01-29 08:02:20 +0200

    Create day-06.js

---
+++ day-06.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/maximum-multiple/train/javascript
+function maxMultiple(divisor, bound) {
+   return Math.floor(bound / divisor) * divisor;
+}
+
+//https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+function hero(bullets, dragons) {
+   return dragons * 2 <= bullets ? true : false;
+}
+
+//https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+function unusualFive() {
+   const returnFive = "Hello";
+   return returnFive.length;
+}
