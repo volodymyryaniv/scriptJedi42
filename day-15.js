2ac748dba74a58fe721fcbb69c6f631b9973eb81
volodymyryaniv <89596689+volodymyryaniv@users.noreply.github.com>
2022-02-08 11:36:43 +0200

    Create day-15.js

---
+++ day-15.js
@@ -0,0 +1,39 @@
+//https://www.codewars.com/kata/572ab0cfa3af384df7000ff8/train/javascript
+function shuffleIt(arr,...arrays){
+  arrays.map(el => {
+    let [first,second] = [el[0],el[1]];
+    [arr[first],arr[second]] = [arr[second],arr[first]]
+    return arr;
+  })
+  return arr;
+}
+
+//https://www.codewars.com/kata/5fd8aa5743b49e0012d43e50/train/javascript
+function loopArr(arr, direction, steps) {
+   for (let i = 0; i < steps; i++) {
+      direction === "left" ? arr.push(arr.shift()) : arr.unshift(arr.pop());
+   }
+   return arr;
+}
+
+//https://www.codewars.com/kata/572af273a3af3836660014a1/train/javascript
+function infiniteLoop(arr, d, n) {
+   let length = arr.length;
+   for (let i = 0; i < n; i++) {
+      switch (d) {
+         case "left":
+            arr[length - 1].push(arr[0].shift());
+            for (let j = length - 1; j > 0; j--) {
+               arr[j - 1].push(arr[j].shift());
+            }
+            break;
+         default:
+            arr[0].unshift(arr[length - 1].pop());
+            for (let j = 0; j < length - 1; j++) {
+               arr[j + 1].unshift(arr[j].pop());
+            }
+            break;
+      }
+   }
+   return arr;
+}
