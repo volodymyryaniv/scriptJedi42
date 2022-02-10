77b95616891cfbfcd3ec14c0bf3b12f797e1ed35
volodymyryaniv <89596689+volodymyryaniv@users.noreply.github.com>
2022-02-10 21:38:10 +0200

    Create day-20.js

---
+++ day-20.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/573bca07dffc1aa693000139/train/javascript
+const regex=/^\-?9\d*0{4,}$/  
+
+//https://www.codewars.com/kata/573d11c48b97c0ad970002d4/train/javascript
+const regex=/https?\:\/\/[\w+\.]+\d?\.(com\.net|com|net)/gi
