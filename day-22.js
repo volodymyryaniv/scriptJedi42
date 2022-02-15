75dd202de5487cc6cd5a99296650b182cd3b71bc
volodymyryaniv <89596689+volodymyryaniv@users.noreply.github.com>
2022-02-15 13:56:20 +0200

    Create day-22.js

---
+++ day-22.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/559f860f8c0d6c7784000119/train/javascript
+function anyArrows(arrows){
+   return arrows.some(arrow => !arrow.damaged);
+}
+
+//https://www.codewars.com/kata/586909e4c66d18dd1800009b/train/javascript
+const multiplyAll = (numbArray) => (integer) =>
+   numbArray.map((number) => integer * number);
