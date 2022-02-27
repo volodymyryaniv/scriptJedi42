9210503596dc645531cf0b38d41c6e69c3719a9b
volodymyryaniv <89596689+volodymyryaniv@users.noreply.github.com>
2022-02-27 22:53:53 +0200

    Update day-35.js

--- day-35.js
+++ day-35.js
@@ -1,5 +1,11 @@
 //https://www.codewars.com/kata/557e508a47c7e9adf9000062/train/javascript
-Object.prototype.description = "TODO: GIVE ME A DESCRIPTION" 
+const prototype = {
+  description : "TODO: GIVE ME A DESCRIPTION"
+}
+
+widget.__proto__ = prototype;
+gadget.__proto__ = prototype;
+thingamabob.__proto__ = prototype;
 
 //https://www.codewars.com/kata/513e1e47c600c93cef000001/train/javascript
 function Animal(name, type) {
